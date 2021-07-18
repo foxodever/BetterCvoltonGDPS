a2f24febc47beca6959b5a786d6ac3982cb1379a
APTKOP <52624723+APTKOP@users.noreply.github.com>
2021-07-18 20:58:53 +0300

    Add files via upload

---
+++ accounts/lostpassword.php
@@ -0,0 +1,65 @@
+Enter your account email<br><form action="lostpassword.php" method="post">Email: <input type="text" name="email"><br><input type="submit" value="Reset"></form>
+<?php
+include "../incl/lib/connection.php";
+include "../incl/lib/mail.php";
+$email = $_POST["email"];
+if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $query = $db->prepare("SELECT * FROM accounts WHERE email = :email");
+    $query->execute([':email' => $email]);
+    if ($query->rowCount() > 0) {
+        $query = $db->prepare("SELECT accountID FROM accounts WHERE email = :email");
+        $query->execute([':email' => $email]);
+        $accountID = $query->fetchColumn();
+        function generate($how_long) {
+            $length = $how_long;
+            $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
+            $numChars = strlen($chars);
+            $string = '';
+            for ($i = 0; $i < $length; $i++) {
+                $string .= substr($chars, rand(1, $numChars) - 1, 1);
+            }
+            return $string;
+        }
+        $pass1 = generate(8);
+        $token = generate(8);
+        $pass = password_hash($pass1, PASSWORD_DEFAULT);
+        $query = $db->prepare("INSERT INTO reset (acc, password, token)
+        VALUES (:acc, :password, :token)");
+        $query->execute([':acc' => $accountID, ':password' => $pass, ':token' => $token]);
+        require("mail/PHPMailerAutoload.php");
+        $mail = new PHPMailer;
+        $mail->CharSet = 'utf-8';
+
+        $mail->isSMTP();
+        $mail->Host = $smtp;
+        $mail->SMTPAuth = true;
+        $mail->Username = $mail_server;
+        $mail->Password = $mail_server_password; 
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = $smtp_port;
+
+        $mail->setFrom($mail_server);
+        $mail->addAddress("$email"); 
+        $mail->isHTML(true);
+        $mail->Subject = 'GDPS password reseting';
+        $mail->Body    = "<h1 align=center>Hello $userName</h1><p align=center>Update your GDPS account password to $pass1 by going to link down:</p>
+        <p align=center><a href='$url_reset?token=$token' 'style=color:blue;text-decoration:none'>Update your account password</a></p>
+        <p align=center>Can not open link? $url_reset?token=$token</p>";
+        $mail->AltBody = '';
+        if($mail->send()) {
+            echo "1";
+        } else {
+            echo "-1";
+        }
+    } else {
+        echo "Email not found";
+    }
+} else {
+    if(empty($_POST["email"])) {
+
+    } else {
+        echo "Email invalid";
+    }
+}
+
+?>
---
+++ accounts/registerGJAccount.php
@@ -0,0 +1,85 @@
+<?php
+error_reporting(E_ALL);
+include "../incl/lib/connection.php";
+include "../incl/lib/mail.php";
+require_once "../incl/lib/exploitPatch.php";
+$ep = new exploitPatch();
+if($_POST["userName"] != ""){
+	//here im getting all the data
+	$userName = $ep->remove($_POST["userName"]);
+	$password = $ep->remove($_POST["password"]);
+	$email = $ep->remove($_POST["email"]);
+	$secret = "";
+	//checking if name is taken
+	$query2 = $db->prepare("SELECT count(*) FROM accounts WHERE userName LIKE :userName");
+	$query2->execute([':userName' => $userName]);
+	$regusrs = $query2->fetchColumn();
+	if ($regusrs > 0) {
+		echo "-2";
+	}else{
+		//checking if name is taken in not registered accounts
+		$query2 = $db->prepare("SELECT count(*) FROM register WHERE userName LIKE :userName");
+		$query2->execute([':userName' => $userName]);
+		$regusrs = $query2->fetchColumn();
+		if ($regusrs > 0) {
+			echo "-2";
+		}else{
+			//checking if email is taken
+			$query2 = $db->prepare("SELECT count(*) FROM accounts WHERE email LIKE :email");
+			$query2->execute([':email' => $email]);
+			$regusrs = $query2->fetchColumn();
+			if ($regusrs > 0) {
+				exit("-3");
+			}else{
+				$query2 = $db->prepare("SELECT count(*) FROM register WHERE email LIKE :email");
+				$query2->execute([':email' => $email]);
+				$regusrs = $query2->fetchColumn();
+				if ($regusrs > 0) {
+					exit("-3");
+				}else{
+					function generate($how_long) {
+						$length = $how_long;
+						$chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
+						$numChars = strlen($chars);
+						$string = '';
+						for ($i = 0; $i < $length; $i++) {
+							$string .= substr($chars, rand(1, $numChars) - 1, 1);
+						}
+						return $string;
+					}
+					$hashpass = password_hash($password, PASSWORD_DEFAULT);
+					$token = generate(10);
+					$query = $db->prepare("INSERT INTO register (userName, password, email, secret, saveData, registerDate, saveKey, token)
+					VALUES (:userName, :password, :email, :secret, '', :time, '', :token)");
+					$query->execute([':userName' => $userName, ':password' => $hashpass, ':email' => $email, ':secret' => $secret, ':token' => $token, ':time' => time()]);
+					require("mail/PHPMailerAutoload.php");
+					$mail = new PHPMailer;
+					$mail->CharSet = 'utf-8';
+		
+					$mail->isSMTP();
+					$mail->Host = $smtp;
+					$mail->SMTPAuth = true;
+					$mail->Username = $mail_server;
+					$mail->Password = $mail_server_password; 
+					$mail->SMTPSecure = 'ssl';
+					$mail->Port = $smtp_port;
+		
+					$mail->setFrom($mail_server);
+					$mail->addAddress("$email"); 
+					$mail->isHTML(true);
+					$mail->Subject = 'GDPS account activation';
+					$mail->Body    = "<h1 align=center>Hello $userName</h1><p align=center>activate your GDPS account by going to link down:</p>
+					<p align=center><a href='$url_register?token=$token' 'style=color:blue;text-decoration:none'>Activate your account</a></p>
+					<p align=center>Can not open link? $url_register?token=$token</p>";
+					$mail->AltBody = '';
+					if($mail->send()) {
+						echo "1";
+					} else {
+						echo "-1";
+					}
+				}
+			}
+		}
+	}
+}
+?>
