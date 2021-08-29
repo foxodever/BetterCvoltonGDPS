4932beadd0418fc9d4a55b6fe263aac61f3da70c
foxodever <52624723+foxodever@users.noreply.github.com>
2021-08-29 15:39:58 +0300

    Update suggestGJStars.php

--- incl/levels/suggestGJStars.php
+++ incl/levels/suggestGJStars.php
@@ -13,29 +13,6 @@ $feature = $ep->remove($_POST["feature"]);
 $levelID = $ep->remove($_POST["levelID"]);
 $accountID = $ep->remove($_POST["accountID"]);
 if($accountID != "" AND $gjp != ""){
-	$status = $gs->getRateStatus($levelID);
-	$cp = 0;
-	if($status == 0) {
-		if($feature == 1) {
-			$cp = 2;
-		} else {
-			$cp = 1;
-		}
-	} else if($status == 1) {
-		if($feature == 1) {
-			$cp = 1;
-		} else {
-			$cp = 0;
-		}
-	} else if($status == 2) {
-		if($feature == 1) {
-			$cp = 0;
-		} else {
-			$cp = -1;
-		}
-	} else if($status == 3) {
-		exit("-1");
-	}
 	$GJPCheck = new GJPCheck();
 	$gjpresult = $GJPCheck->check($gjp,$accountID);
 	if($gjpresult == 1){
@@ -44,12 +21,8 @@ if($accountID != "" AND $gjp != ""){
 			$gs->rateLevel($accountID, $levelID, $stars, $difficulty["diff"], $difficulty["auto"], $difficulty["demon"]);
 			$gs->featureLevel($accountID, $levelID, $feature);
 			$gs->verifyCoinsLevel($accountID, $levelID, 1);
-			if($cp != 0) {
-				$a = $gs->addCreatorPoints($cp, $levelID);
-			} else {
-				$a = -1;
-			}
-			echo $a;
+			echo 1;
+			include "cp.php";
 		}else if($gs->checkPermission($accountID, "actionSuggestRating")){
 			$gs->suggestLevel($accountID, $levelID, $difficulty["diff"], $stars, $feature, $difficulty["auto"], $difficulty["demon"]);
 			echo 1;
@@ -62,5 +35,4 @@ if($accountID != "" AND $gjp != ""){
 }else{
 	echo -2;
 }
-include "cp.php";
 ?>
