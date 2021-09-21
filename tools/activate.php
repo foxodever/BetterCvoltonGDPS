a35079928ac4a3b07f5eba72c9f9a71e94e4baf1
foxodever <52624723+foxodever@users.noreply.github.com>
2021-09-21 13:02:33 +0300

    Update activate.php

--- tools/activate.php
+++ tools/activate.php
@@ -2,11 +2,54 @@
 include "../incl/lib/connection.php";
 require_once "../incl/lib/mainLib.php";
 $gs = new mainLib();
-$tk = $_GET["token"];
-if(!empty($tk)) {
-    $query = $db->prepare("SELECT * FROM register WHERE token = :tk");
-    $query->execute([':tk' => $tk]);
-    if ($query->rowCount() > 0) {
+session_start();
+function invalid() {
+    exit("-1");
+}
+function success() {
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {
+            font-family: Geneva, Arial, Helvetica, sans-serif;
+            margin: 0;
+        }
+        .main {
+            margin-top: 100px;
+        }
+        form {
+            margin: 5px;
+        }
+    </style>
+    <div class="main" align="center">
+        <h1>Account activated!</h1>
+        <p>You can login now</p>
+    </div>';
+    exit;
+}
+$captcha = $_SESSION["captcha"];
+$tk = htmlspecialchars($_GET["token"]);
+if($tk != "") {
+    $_SESSION["tk"] = $tk;
+    echo "<script>location.href='activate.php'</script>";
+    exit;
+} else {
+    if($_SESSION["tk"]) {
+        $query = $db->prepare("SELECT * FROM register WHERE token = :tk");
+        $query->execute([':tk' => $_SESSION["tk"]]);
+        if ($query->rowCount() == 0) {
+            invalid();
+        }
+    } else {
+        invalid();
+    }
+}
+$cap = htmlspecialchars($_POST["cap"]);
+
+if($cap == "") {
+
+} else {
+    if($cap == $captcha) {
+        $tk = $_SESSION["tk"];
         $query = $db->prepare("SELECT userName FROM register WHERE token = :tk");
         $query->execute([':tk' => $tk]);
         $userName = $query->fetchColumn();
@@ -23,11 +66,37 @@ if(!empty($tk)) {
         $query->execute([':userName' => $userName, ':password' => $password, ':email' => $email, ':registerDate' => $registerDate]);
         $query = $db->prepare("DELETE FROM register WHERE token = :tk");
         $query->execute([':tk' => $tk]);
-        echo "1";
+        success();
     } else {
-        echo "-1";
+        $err = "Invalid captcha";
     }
-} else {
-    echo "1";
 }
 ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    * {
+        font-family: Geneva, Arial, Helvetica, sans-serif;
+        margin: 0;
+    }
+    .main {
+        margin-top: 100px;
+    }
+    form {
+        margin: 5px;
+    }
+</style>
+<div class="main" align="center">
+    <h1>Almsot here!</h1>
+    <p style="padding-bottom: 10px"><?php
+    if(!$err) {
+        echo "Please verify what you are not a robot!";
+    } else {
+        echo $err;  
+    }
+    ?></p>
+    <img style="border: 1px solid black" src="cap.php" method="post">
+    <form action="activate.php?token=<?=$tk?>" method="post">
+        <input style="padding-bottom: 5px" name="cap" placeholder="Enter here..." /><br />
+        <input type="submit" value="verify" />
+    </form>
+</div>
