ecc33ed75e2185370408b98f7917ebb406c49437
foxodever <52624723+foxodever@users.noreply.github.com>
2021-09-21 13:14:14 +0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -2,6 +2,7 @@
 $smtp = ""; //smtp server adress
 $mail_server = ""; //mail adress
 $mail_server_password = ""; //mail password
+$mail_type = 'ssl'; // choose ssl or tls
 $smtp_port = 465; //smtp port
 $url_register = "DOMAIN/tools/activate.php";
 $url_reset = "DOMAIN/tools/reset.php"; //enter url to your activate account file. Example: https://example.com/database/activate.php
