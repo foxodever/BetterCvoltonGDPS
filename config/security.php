f88a2ed90f55bae372f12cceb318330108f87652
foxodever <52624723+foxodever@users.noreply.github.com>
2021-11-23 12:11:53 +0300

    hCaptcha settings

--- config/security.php
+++ config/security.php
@@ -1,4 +1,10 @@
 <?php
+$captchaType = 1; // 1 - classic, 2 - hcaptcha
+if($captchaType == 2) {
+    $hcaptcha_key = ""; // key
+    $hcaptcha_secret = ""; // starts with 0x
+}
+
 $cloudSaveEncryption = 0; //0 = password string replacement, 1 = cloud save encryption (password dependant)
 $sessionGrants = 1; //0 = GJP check is done every time; 1 = GJP check is done once per hour; drastically improves performance, slightly descreases security
 ?>
