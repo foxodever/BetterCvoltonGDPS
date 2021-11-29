bcbe2a895e34307001214296dd94ea0d88d71d35
foxodever <52624723+foxodever@users.noreply.github.com>
2021-11-29 21:48:04 +0300

    Sussy Baka

--- config/security.php
+++ config/security.php
@@ -1,8 +1,12 @@
 <?php
-$captchaType = 1; // 1 - classic, 2 - hcaptcha
+$captchaType = 1; // 1 - classic, 2 - hcaptcha, 3 - reCaptcha
 if($captchaType == 2) {
     $hcaptcha_key = ""; // key
     $hcaptcha_secret = ""; // starts with 0x
+}
+if($captchaType == 3) {
+    $recaptcha_key = "";
+    $recaptcha_secret = ""; // SECRET not PUBLIC
 }
 
 $cloudSaveEncryption = 0; //0 = password string replacement, 1 = cloud save encryption (password dependant)
