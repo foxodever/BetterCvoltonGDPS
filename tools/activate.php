634a14ab34c7b4a08b35f47fe54732d31b66cbee
foxodever <52624723+foxodever@users.noreply.github.com>
2022-06-20 18:07:41 +0300

    Update activate.php

--- tools/activate.php
+++ tools/activate.php
@@ -3,7 +3,6 @@ include "../incl/lib/connection.php";
 include "../config/security.php";
 require_once "../incl/lib/mainLib.php";
 $gs = new mainLib();
-session_start();
 function invalid() {
     exit("-1");
 }
@@ -29,21 +28,6 @@ function success() {
 }
 $captcha = $_SESSION["captcha"];
 $tk = htmlspecialchars($_GET["token"]);
-if($tk != "") {
-    $_SESSION["tk"] = $tk;
-    echo "<script>location.href='activate.php'</script>";
-    exit;
-} else {
-    if($_SESSION["tk"]) {
-        $query = $db->prepare("SELECT * FROM register WHERE token = :tk");
-        $query->execute([':tk' => $_SESSION["tk"]]);
-        if ($query->rowCount() == 0) {
-            invalid();
-        }
-    } else {
-        invalid();
-    }
-}
 $cap = htmlspecialchars($_POST["cap"]);
 
 if($cap == "" && $captchaType == 1) {
@@ -62,7 +46,7 @@ if($cap == "" && $captchaType == 1) {
         $response = curl_exec($ch);
         $responseData = json_decode($response);
         if($responseData->success) {
-            $tk = $_SESSION["tk"];
+            $tk = htmlspecialchars($_GET["token"]);
             $query = $db->prepare("SELECT userName FROM register WHERE token = :tk");
             $query->execute([':tk' => $tk]);
             $userName = $query->fetchColumn();
@@ -106,7 +90,7 @@ if($cap == "" && $captchaType == 1) {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
         if(json_decode($response, true)["success"] == "1") {
-            $tk = $_SESSION["tk"];
+            $tk = htmlspecialchars($_GET["token"]);
             $query = $db->prepare("SELECT userName FROM register WHERE token = :tk");
             $query->execute([':tk' => $tk]);
             $userName = $query->fetchColumn();
@@ -131,7 +115,7 @@ if($cap == "" && $captchaType == 1) {
         }
     }
     if($cap == $captcha && $captchaType == 1) {
-        $tk = $_SESSION["tk"];
+            $tk = htmlspecialchars($_GET["token"]);
         $query = $db->prepare("SELECT userName FROM register WHERE token = :tk");
         $query->execute([':tk' => $tk]);
         $userName = $query->fetchColumn();
@@ -194,21 +178,21 @@ if($cap == "" && $captchaType == 1) {
     if($captchaType == 1) {
     ?>
     <img style="border: 1px solid black" src="cap.php" method="post">
-    <form action="activate.php?token=<?=$tk?>" method="post">
+    <form action="" method="post">
         <input style="padding-bottom: 5px" name="cap" placeholder="Enter here..." /><br />
         <input type="submit" value="verify" />
     </form>
     <?php
     } else if($captchaType == 2) {
         ?>
-        <form action="activate.php?token=<?=$tk?>" method="post">
+        <form action="" method="post">
             <div class="h-captcha" data-sitekey="<?=$hcaptcha_key?>"></div><br />
             <input type="submit" value="verify" />
         </form>
         <?php
     } else {
         ?>
-        <form action="activate.php?token=<?=$tk?>" method="post">
+        <form action="" method="post">
             <div class="g-recaptcha" data-sitekey="<?=$recaptcha_key?>"></div>
             <input type="submit" value="verify" />
         </form>
